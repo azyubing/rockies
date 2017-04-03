bcf680247d215bba22f38263bff6f0aa4c99b56a
“Ruby” <power888@sina.com>
2017-04-03 15:52:43 +0800

    Initialize project

---
+++ rockiesdatahub/src/main/java/com/rockies/webservice/Application.java
@@ -0,0 +1,14 @@
+package com.rockies.webservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
